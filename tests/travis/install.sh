a68a294b9ca7f541bd7692a3fa4b32c5af13609c
pkruithof <pkruithof@gmail.com>
2020-11-05 10:14:20 +0100

    Try out Github Actions

--- tests/travis/install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-trap 'exit' ERR
-
-cd "$(dirname "$0")"/../../
-
-echo yes | pecl install amqp
-
-if [ "$SYMFONY_VERSION" != "" ]; then composer require "symfony/symfony:${SYMFONY_VERSION}" --no-update; fi;
